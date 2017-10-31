2eaedd74ebf7d77521d25fe487ace264367e2cd9
Jiwon Park <jiiwon0@gmail.com>
2017-10-31 13:34:23 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+// app: represent express server, creating brand new handler
+// .get: watch for incoming HTTP request, get info
+// '/': root route
+// req: object representing the incoming request
+// res: object representing the outgoing response
+// res.send: send some JSON back to the request
+// .listen: listen to incoming traffic on port
