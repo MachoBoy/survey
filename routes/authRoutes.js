56ac1e7bf42ebbb54bb16af2e1f8f8d4de764e25
Jiwon Park <jiiwon0@gmail.com>
2017-11-02 22:43:25 -0700

    finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,12 @@ module.exports = app => {
   // second route handler user visits
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  // logout route handler
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   // third route handler
   app.get('/api/current_user', (req, res) => {
     res.send(req.user); //get access to user
