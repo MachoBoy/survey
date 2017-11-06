5f9fb2457e25d3a6d3f2ddd73afc17b20a2c65b8
Jiwon Park <jiiwon0@gmail.com>
2017-11-06 14:51:52 -0800

    deploy

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 
-const Dashboard = () => <h2>dashboard</h2>;
+const Dashboard = () => <h2>dashboards</h2>;
 const SurveyNew = () => <h2>surverynew</h2>;
 
 class App extends Component {
